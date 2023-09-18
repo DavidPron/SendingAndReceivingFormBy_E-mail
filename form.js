ede077985299b719b6d0ee14f3b56f85de895078
David Pron <92371541+DavidPron@users.noreply.github.com>
2023-09-18 12:02:08 +0300

    Update form.js

--- form.js
+++ form.js
@@ -1,15 +1,15 @@
-// $('form').submit(function(e) {
-//     e.preventDefault();
-//     $.ajax({
-//         type: "POST",
-//         url: "mailer/smart.php",
-//         data: $(this).serialize()
-//     }).done(function() {
-//         $(this).find("input").val("");
-//         $('#consultation, #order').fadeOut();
-//         $('.overlay, #thanks').fadeIn('slow');
+ $('form').submit(function(e) {
+     e.preventDefault();
+     $.ajax({
+         type: "POST",
+         url: "mailer/smart.php",
+         data: $(this).serialize()
+     }).done(function() {
+         $(this).find("input").val("");
+         $('#consultation, #order').fadeOut();
+         $('.overlay, #thanks').fadeIn('slow');
 
-//         $('form').trigger('reset');
-//     });
-//     return false;
-// });
+         $('form').trigger('reset');
+     });
+     return false;
+});
